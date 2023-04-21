64c2ad20209d1e58b377c1f65ddc544f1a596bb8
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-04-21 22:08:11 +0300

    Create compareWordDifferences.js

---
+++ compareWordDifferences.js
@@ -0,0 +1,43 @@
+function wordsComparison(input) {
+    let [firstWord, secondWord] = [...input];
+    firstChars = [...firstWord];
+    secondChars = [...secondWord];
+    let firstLettersCount = {};
+    let secondLettersCount = {};
+    let diff = 0;
+ 
+    for (let letter of firstWord) {
+        if (firstLettersCount.hasOwnProperty(letter)) {
+            firstLettersCount[letter]++;
+        } else {
+            firstLettersCount[letter] = 1;
+        }
+    }
+ 
+    for (let letter of secondWord) {
+        if (secondLettersCount.hasOwnProperty(letter)) {
+            secondLettersCount[letter]++;
+        } else {
+            secondLettersCount[letter] = 1;
+        }
+    }
+ 
+    for (let key in firstLettersCount) {
+        if (!secondLettersCount.hasOwnProperty(key)) {
+            diff += firstLettersCount[key];
+        } else {
+            if (firstLettersCount[key] !== secondLettersCount[key]) {
+                diff += Math.abs(firstLettersCount[key] - secondLettersCount[key]);
+            }
+        }
+    }
+    for (let key in secondLettersCount) {
+        if (!firstLettersCount.hasOwnProperty(key)) {
+            diff += secondLettersCount[key];
+        }
+    }
+    console.log(diff)
+ 
+}
+ 
+wordsComparison(['codewars', 'hackerrank'])
