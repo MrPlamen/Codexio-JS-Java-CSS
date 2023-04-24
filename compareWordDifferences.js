475060ed8bb112fff6afab8a9ff118df4aaf955b
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-04-24 19:47:35 +0300

    Update compareWordDifferences.js

--- compareWordDifferences.js
+++ compareWordDifferences.js
@@ -1,46 +1,59 @@
 function wordsComparison(input) {
     let [firstWord, secondWord] = [...input];
-    firstChars = [...firstWord];
-    secondChars = [...secondWord];
-    let firstLettersCount = {};
-    let secondLettersCount = {};
-    let diff = 0;
- 
-    for (let letter of firstWord) {
-        if (firstLettersCount.hasOwnProperty(letter)) {
-            firstLettersCount[letter]++;
-        } else {
-            firstLettersCount[letter] = 1;
-        }
-    }
- 
-    for (let letter of secondWord) {
-        if (secondLettersCount.hasOwnProperty(letter)) {
-            secondLettersCount[letter]++;
-        } else {
-            secondLettersCount[letter] = 1;
-        }
-    }
- 
-    for (let key in firstLettersCount) {
-        if (!secondLettersCount.hasOwnProperty(key)) {
-            diff += firstLettersCount[key];
-        } else {
-            if (firstLettersCount[key] !== secondLettersCount[key]) {
-                diff += Math.abs(firstLettersCount[key] - secondLettersCount[key]);
+
+    let firstChars = [...firstWord];
+    let secondChars = [...secondWord];
+
+    removeExcessiveLetters(firstChars, secondChars);
+    removeExcessiveLetters(secondChars, firstChars);
+
+    function removeExcessiveLetters(currentCharArray, otherCharArray) {
+
+        for (let letter of currentCharArray) {
+            if (!otherCharArray.includes(letter)) {
+
+                removeLetters(currentCharArray, letter);
+
+            } else {
+                let firstCharsOccurences = currentCharArray.filter(x => x == letter).length;
+                let secondCharsOccurences = otherCharArray.filter(x => x == letter).length;
+
+                let differenceInCount = firstCharsOccurences - secondCharsOccurences;
+
+                while (differenceInCount > 0) {
+                    removeLetters(currentCharArray, letter);
+                    differenceInCount--;
+                }
+
+                while (differenceInCount < 0) {
+                    removeLetters(otherCharArray, letter);
+                    differenceInCount++;
+                }
             }
         }
     }
-    for (let key in secondLettersCount) {
-        if (!firstLettersCount.hasOwnProperty(key)) {
-            diff += secondLettersCount[key];
-        }
-    }
-    
-    return diff;
- 
+
+    // These are the resulting words, if needed: 
+
+    let firstWordFinal = firstChars.join('');
+    let secondWordFinal = secondChars.join('');
+
+    // This is only the number of removed words:
+
+    let result = (firstWord.length - firstWordFinal.length);
+    result += (secondWord.length - secondWordFinal.length);
+
+    return result + firstWordFinal + secondWordFinal;
+
 }
- 
+
+function removeLetters(array, letter) {
+
+    let index = array.indexOf(letter);
+    array.splice(index, 1, '');
+
+}
+
 const wordInput = ['codewars', 'hackerrank'];
- 
+
 console.log(wordsComparison(wordInput));
