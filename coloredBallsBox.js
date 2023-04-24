de95e2c42ace1ce3cbb8a2288c2cf295706787e5
Plamen Petrov <99687005+MrPlamen@users.noreply.github.com>
2023-04-24 21:50:09 +0300

    Update coloredBallsBox.js

--- coloredBallsBox.js
+++ coloredBallsBox.js
@@ -1,40 +1,43 @@
 function coloredBalls(data) {
- 
-    let [...input] = data;
+    const input = [...data];
     let box = [];
     let counter = 0;
- 
-    for (let entry of input) {
-        if (entry === 'End') {
-            break;
-        }
-        let [color, count] = entry.split(': ');
-        for (let i = 0; i < count; i++) {
-            box.push(color);
-        }
-    }
- 
+
+    readInput(input,box);
+
     for (let ball in box) {
-        while(!onlyOneColor()) {
+        while (!onlyFirstColorLeft(box)) {
             box.pop();
             counter++;
         }
     }
- 
+
     return counter;
- 
-    function onlyOneColor() {
-        let pickedColor = box[0];
-        for (let ball in box) {
-            if (box[ball] !== pickedColor) {
-                return false;
-            }
-        }
-        return true;
-    }
- 
 }
- 
-const input = ['Red: 5', 'Blue: 3','Yellow: 2','Green: 3','End'];
- 
+
+function readInput(input,box) {
+    for (let entry of input) {
+        if (entry === 'End') {
+            break;
+        }
+
+        let [color, count] = entry.split(': ');
+
+        for (let i = 0; i < count; i++) {
+            box.push(color);
+        }
+    }
+}
+
+function onlyFirstColorLeft(box) {
+    let firstColor = box[0];
+    for (let ball in box) {
+        if (box[ball] !== firstColor) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const input = ['Red: 5', 'Blue: 3', 'Yellow: 2', 'Green: 3', 'End'];
 console.log(coloredBalls(input));
